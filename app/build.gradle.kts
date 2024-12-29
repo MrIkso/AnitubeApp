c068f558c6a2b81b1300396f9f55f37914c67607
MrIkso <mrikso821@gmail.com>
2024-12-29 20:50:55 +0200

    feat: bump version to 1.0.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.mrikso.anitube.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.0.4"
+        versionCode = 6
+        versionName = "1.0.5"
     }
 
     if (keystorePropertiesFile.exists()) {
