436c1d57f0baf0be2b2e50d98ed7f563ef99c88e
MrIkso <mrikso821@gmail.com>
2025-05-27 22:14:05 +0300

    fix: fixed glide okhttpclient

--- app/src/main/java/com/mrikso/anitube/app/di/MyAppGlideModule.java
+++ app/src/main/java/com/mrikso/anitube/app/di/MyAppGlideModule.java
@@ -10,6 +10,9 @@ import com.bumptech.glide.annotation.GlideModule;
 import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader;
 import com.bumptech.glide.load.model.GlideUrl;
 import com.bumptech.glide.module.AppGlideModule;
+import com.mrikso.anitube.app.BuildConfig;
+import com.mrikso.anitube.app.network.ApiClient;
+import com.mrikso.anitube.app.network.UserAgentInterceptor;
 
 import java.io.InputStream;
 import java.util.concurrent.TimeUnit;
@@ -22,7 +25,8 @@ public final class MyAppGlideModule extends AppGlideModule {
     @Override
     public void registerComponents(@NonNull Context context, @NonNull Glide glide, @NonNull Registry registry) {
         OkHttpClient client = new OkHttpClient.Builder()
-                .addInterceptor(new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+                .addInterceptor(new UserAgentInterceptor(ApiClient.DESKTOP_USER_AGENT))
+                .addInterceptor(new HttpLoggingInterceptor().setLevel(BuildConfig.DEBUG ? HttpLoggingInterceptor.Level.BASIC : HttpLoggingInterceptor.Level.NONE))
                 .connectTimeout(15, TimeUnit.SECONDS)
                 .readTimeout(15, TimeUnit.SECONDS)
                 .writeTimeout(15, TimeUnit.SECONDS)
