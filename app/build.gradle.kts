82428442b6900540a737ab90ff597a75be96c6c1
MrIkso <mrikso821@gmail.com>
2025-12-04 01:42:33 +0200

    feat: bump version to 1.0.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,10 +25,10 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 7
-        versionName = "1.0.6"
+        versionName = "1.0.7"
         ndk {
             //noinspection ChromeOsAbiSupport
-            //  abiFilters += listOf("armeabi-v7a", "arm64-v8a")
+            abiFilters += listOf("armeabi-v7a", "arm64-v8a")
         }
         loadConfig("secret.properties")
     }
