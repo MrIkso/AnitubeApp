874a6e8d92a99933349604310f6dc8e11fc19cc1
gaguuuuu <githuberrrgithuber@gmail.com>
2026-09-20 22:26:28 +0900

    Update ApiClient.java

--- app/src/main/java/com/mrikso/anitube/app/network/ApiClient.java
+++ app/src/main/java/com/mrikso/anitube/app/network/ApiClient.java
@@ -6,11 +6,11 @@ public class ApiClient {
     public static final String MOBILE_USER_AGENT =
             "Mozilla/5.0 (Linux; Android 13; SM-M526B) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.5615.135 Mobile Safari/537.36 OPR/75.2.3978.72468";
 
-    public static final String BASE_URL = "https://anitube.in.ua";
+    public static final String BASE_URL = "https://anitube.biz";
     public static final String RANDOM_ANIME_URl = BASE_URL + "/?do=random_anime";
-    public static final String PROFILE_BG_URL = "https://anitube.in.ua/templates/smartphone/images/profile_bg.jpg";
+    public static final String PROFILE_BG_URL = "https://anitube.biz/templates/smartphone/images/profile_bg.jpg";
     public static final String ANIME_CAROUSEL_BG_URL =
-            "https://anitube.in.ua/templates/AniTubenew/images/images/slider/slayd2.png";
+            "https://anitube.biz/templates/AniTubenew/images/images/slider/slayd2.png";
     public static final String ANIME_CALLENDAR_URL =
             "https://mrikso.bitbucket.io/AnitubeApp/images/callendar_anime.jpg";
     public static final String ANIME_GENRES_URL = "https://mrikso.bitbucket.io/AnitubeApp/images/genres_anime.jpg";
